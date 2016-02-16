3f0b188df6ef329d38f78b225731d63ff784366b
gcwolfe007 <gcwolfe007@gmail.com>
2016-02-16 14:07:01 -0500

    added Example for advanced reduce

--- Part4/example.js
+++ Part4/example.js
@@ -1,12 +1,12 @@
 var fs = require('fs')
 
 
- var output = fs.readFileSync('data.txt', 'utf8')
- .trim()
- .split('\n')
- .map(line => line.split('\t'))
- 
- console.log('output',output)
+var output = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'))
+
+console.log('output', output)
  
  
   
