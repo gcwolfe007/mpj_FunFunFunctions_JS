4f4db09274d302ba7c7717a08367840b200d4afb
gcwolfe007 <gcwolfe007@gmail.com>
2016-02-16 14:47:17 -0500

    end of video

--- Part4/example.js
+++ Part4/example.js
@@ -1,12 +1,21 @@
 var fs = require('fs')
 
-
 var output = fs.readFileSync('data.txt', 'utf8')
     .trim()
     .split('\n')
     .map(line => line.split('\t'))
+    .reduce((customers,line)=>{
+       // console.log(line)
+       customers[line[0]] = customers[line[0]] || []
+       customers[line[0]].push({
+           name: line[1],
+           price:line[2],
+           quantity:line[3]
+       })
+        return customers
+    },{})
 
-console.log('output', output)
+console.log('output', JSON.stringify(output,null,2))
  
  
   
